5acfc4c15dbf7045615cfbcc50b003cc2407a2a0
kruthika749 <ammukruthika151@gmail.com>
2026-09-13 22:11:46 +0530

    Add grievance search by location

--- src/main/java/com/smartgrievance/smart_grievance/GrievanceController.java
+++ src/main/java/com/smartgrievance/smart_grievance/GrievanceController.java
@@ -3,6 +3,7 @@ package com.smartgrievance.smart_grievance;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,12 @@ public class GrievanceController {
     @GetMapping("/grievances")
     public List<Grievance> getAllGrievances() {
         return grievanceRepository.findAll();
+    }
+
+    @GetMapping("/grievances/location/{location}")
+    public List<Grievance> getGrievancesByLocation(
+            @PathVariable String location) {
+
+        return grievanceRepository.findByLocationIgnoreCase(location);
     }
 }
--- src/main/java/com/smartgrievance/smart_grievance/GrievanceRepository.java
+++ src/main/java/com/smartgrievance/smart_grievance/GrievanceRepository.java
@@ -1,7 +1,11 @@
 package com.smartgrievance.smart_grievance;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GrievanceRepository extends JpaRepository<Grievance, Integer> {
+
+    List<Grievance> findByLocationIgnoreCase(String location);
 
 }
