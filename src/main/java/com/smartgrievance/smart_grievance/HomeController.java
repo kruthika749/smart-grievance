45ad619ecff36f8202f16bc38ed8b06432e38bf2
Kavya <kavyabasavaraju61@gmail.com>
2026-09-14 18:00:11 +0530

    Set resolvedAt when grievance is resolved

--- src/main/java/com/smartgrievance/smart_grievance/HomeController.java
+++ src/main/java/com/smartgrievance/smart_grievance/HomeController.java
@@ -1,8 +1,15 @@
 package com.smartgrievance.smart_grievance;
 
+import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+
 
 @RestController
 public class HomeController {
@@ -26,6 +33,7 @@ public class HomeController {
         g.setEmail(email);
         g.setGrievance(grievance);
         g.setLocation(location);
+        g.setStatus("Pending");
 
         grievanceRepository.save(g);
 
@@ -35,4 +43,29 @@ public class HomeController {
                 + "Grievance: " + grievance + "<br>"
                 + "Location: " + location;
     }
+    @GetMapping("/grievances")
+public List<Grievance> getGrievances() {
+    return grievanceRepository.findAll();
+}
+
+@PutMapping("/grievances/{id}/status")
+public String updateStatus(
+        @PathVariable int id,
+        @RequestParam String status) {
+
+    Grievance grievance = grievanceRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Grievance not found"));
+
+if ("Resolved".equalsIgnoreCase(status)) {
+    grievance.setResolvedAt(LocalDateTime.now());
+} else {
+    grievance.setResolvedAt(null);
+}
+
+grievance.setStatus(status);
+grievanceRepository.save(grievance);
+
+return "Status updated successfully";
+
+        }
 }
